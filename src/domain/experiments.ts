0e51c2ec3a5610276337770e788d53608bb64ec3
felipelealdefaria <felipelealdefaria@gmail.com>
2021-02-15 13:15:27 -0300

    refactor: remove namespace

--- src/domain/experiments.ts
+++ src/domain/experiments.ts
@@ -1,29 +1,27 @@
 export interface Experiments {
-  init: (params: Experiments.InitParams) => Promise<any>
-  convert: (params: Experiments.ConvertParams) => Promise<void>
-  participate: (params: Experiments.ParticipateParams) => Promise<Experiments.ParticipateResponse>
+  init: (params: InitParams) => Promise<any>
+  convert: (params: ConvertParams) => Promise<void>
+  participate: (params: ParticipateParams) => Promise<ParticipateResponse>
 }
 
-export namespace Experiments {
-  export type InitParams = {
-    baseUrl: string
-  }
+export type InitParams = {
+  baseUrl: string
+}
 
-  export type ConvertParams = {
-    kpi?: string
-    session: any
-    experimentName: string
-  }
+export type ConvertParams = {
+  kpi?: string
+  session: any
+  experimentName: string
+}
 
-  export type ParticipateParams = {
-    session: any
-    traffic?: number
-    variationsName: string[]
-    experimentName: string
-  }
+export type ParticipateParams = {
+  session: any
+  traffic?: number
+  variationsName: string[]
+  experimentName: string
+}
 
-  export type ParticipateResponse = {
-    alternativeName: string
-    experimentName: string
-  }
+export type ParticipateResponse = {
+  alternativeName: string
+  experimentName: string
 }
