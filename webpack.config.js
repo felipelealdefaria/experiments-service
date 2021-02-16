47769ff4104523c0c1092a84141b870e8a38be38
felipelealdefaria <felipelealdefaria@gmail.com>
2021-02-16 11:56:26 -0300

    refactor: config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
+  mode: 'production',
+  entry: './src/index.ts',
   output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist',
     filename: 'bundle.js'
   },
   resolve: {
@@ -14,7 +14,7 @@ module.exports = {
       http: require.resolve('stream-http'),
       buffer: require.resolve('buffer/')
     },
-    extensions: ['.ts', '.tsx', '.js', '.scss'],
+    extensions: ['.ts', '.tsx', '.js'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
