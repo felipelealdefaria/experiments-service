e0520482f8bf91b93c9ce4461e7b8b5cc33936a9
felipelealdefaria <felipelealdefaria@gmail.com>
2021-04-30 11:40:04 -0300

    chore: config jest, tsconfig and lint

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  testMatch: [
+    '<rootDir>/src/**/*.{spec, test}.{ts,tsx}',
+    '<rootDir>/src/**/__tests__/*.{spec, test}.{ts,tsx}'
+  ],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
+    '<rootDir>/src/**/*.ts',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
