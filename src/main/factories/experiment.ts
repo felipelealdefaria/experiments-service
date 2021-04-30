25dd2941af1ac6497638c173eb89614ad9735fbb
felipelealdefaria <felipelealdefaria@gmail.com>
2021-04-30 11:39:09 -0300

    chore: remove unit tests

--- src/main/factories/experiment.ts
+++
@@ -1,6 +0,0 @@
-import { Experiments } from '@/domain/experiments'
-import { ExperimentsService } from '@/data/experiments-service'
-import { makeSixpackExperiments } from '@/main/factories/sixpack'
-
-const makeExperimentService = (): Experiments => new ExperimentsService(makeSixpackExperiments())
-export const experiment = makeExperimentService()
