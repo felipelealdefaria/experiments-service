7dd4cc58a589dfd6df4e7c17d70df98fdda0e9a9
felipelealdefaria <felipelealdefaria@gmail.com>
2021-04-30 11:44:10 -0300

    refactor: create status reponse type

--- src/data/experiments-service.ts
+++ src/data/experiments-service.ts
@@ -7,54 +7,38 @@ export class ExperimentsService implements Exp.Experiments {
     private readonly experiment: ExperimentsParams
   ) {}
 
-  async init ({ baseUrl }: Exp.InitParams): Promise<any> {
+  async init ({ baseUrl, timeout }: Exp.InitParams): Promise<Exp.InitResponse> {
     this.baseUrl = baseUrl
-    const session = await this.experiment.startExperiment({ baseUrl })
-    return session
+    const res: Exp.InitResponse = await this.experiment.startExperiment({ baseUrl: baseUrl, timeout: timeout })
+
+    if (res?.success) return { success: true, session: res?.session }
+    return { error: true, session: null }
   }
 
-  async participate ({
-    traffic,
-    session,
-    variationsName,
-    experimentName
-  }: Exp.ParticipateParams): Promise<Exp.ParticipateResponse> {
+  async participate ({ traffic, session, variationsName, experimentName }: Exp.ParticipateParams): Promise<Exp.ParticipateResponse> {
     if (!session) session = await this.init({ baseUrl: this.baseUrl })
 
     const force = this.forceVariant(`force-${experimentName}`)
+    const res: Exp.ParticipateResponse = await this.experiment.participateExperiment({ force, session, experimentName, variationsName, traffic: traffic || 1 })
 
-    const res = await this.experiment.participateExperiment({
-      force,
-      session,
-      experimentName,
-      variationsName,
-      traffic: traffic || 1
-    })
-
-    return res
+    if (res?.success) return { success: true, experimentName: res?.experimentName, alternativeName: res?.alternativeName }
+    return { error: true, experimentName: null, alternativeName: null }
   }
 
-  async convert ({
-    kpi,
-    session,
-    experimentName
-  }: Exp.ConvertParams): Promise<void> {
+  async convert ({ kpi, session, experimentName }: Exp.ConvertParams): Promise<Exp.ConvertResponse> {
     if (!session) session = await this.init({ baseUrl: this.baseUrl })
+    const res: Exp.ConvertResponse = await this.experiment.convertEvent({ kpi, session, experimentName })
 
-    await this.experiment.convertEvent({
-      kpi,
-      session,
-      experimentName
-    })
+    if (res?.success) return { success: true, kpi: res?.kpi, experimentName: res?.experimentName, alternativeName: res?.alternativeName }
+    return { error: true, message: res?.message }
   }
 
   forceVariant (cookieName: string): any {
     const decodedCookies = decodeURIComponent(document.cookie)
     const cookies = decodedCookies.split(';')
-
     const forceCookie = cookies.find((cookie) => cookie.match(cookieName))
-    if (forceCookie) return forceCookie.replace(`${cookieName}=`, '').trim()
 
+    if (forceCookie) return forceCookie.replace(`${cookieName}=`, '').trim()
     return forceCookie || null
   }
 }
--- src/infra/sixpack-experiments.ts
+++ src/infra/sixpack-experiments.ts
@@ -1,67 +1,56 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import * as Exp from '@/data/experiments-params'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const sixpack = require('sixpack-client')
 
 export class SixpackExperiments implements Exp.ExperimentsParams {
-  async startExperiment ({ baseUrl }: Exp.StartExperimentParams): Promise<any> {
+  async startExperiment ({ baseUrl, timeout }: Exp.StartExperimentParams): Promise<Exp.StartExperimentResponse> {
     try {
-      return new sixpack.Session({
-        timeout: 8000,
-        base_url: baseUrl
-      })
+      const res = await new sixpack.Session({ base_url: baseUrl, timeout: timeout || 8000 })
+      return { success: true, session: res }
     } catch (error) {
       console.error('SIXPACK INIT ERROR: ', error)
+      return { error: true, session: null }
     }
   }
 
-  async participateExperiment ({
-    force,
-    traffic,
-    session,
-    experimentName,
-    variationsName
-  }: Exp.ParticipateParams): Promise<Exp.ParticipateResponse> {
-    return new Promise<any>((resolve, reject) => {
+  async participateExperiment (params: Exp.ParticipateParams): Promise<Exp.ParticipateResponse> {
+    const { force, traffic, session, experimentName, variationsName } = params
+    return new Promise<Exp.ParticipateResponse>((resolve, reject) => {
       try {
-        if (!session) return console.error('[PARTICIPATE] SESSION NOT FOUND')
+        if (!session) return console.error('[PARTICIPATE] SESSION NOT FOUND');
 
-        session.participate(
-          experimentName,
-          variationsName,
-          traffic,
-          force,
-          (error: any, result: any): any => {
-            if (error) return reject(error)
-            if (result) {
-              if (result.status === 'failed') {
-                console.error('[PARTICIPATE] REQUEST ERROR: ', result?.error)
-              }
-              return resolve({
-                experimentName: result?.experiment?.name,
-                alternativeName: result?.alternative?.name
-              })
+        (session as any).participate(experimentName, variationsName, traffic, force, (error: any, result: any): any => {
+          if (error) return reject(error)
+          if (result) {
+            if (result.status === 'failed') {
+              console.error('[PARTICIPATE] REQUEST ERROR: ', result?.error)
+              return resolve({ error: true, message: result?.error })
             }
-          })
+            return resolve({
+              success: true,
+              experimentName: result?.experiment?.name,
+              alternativeName: result?.alternative?.name
+            })
+          }
+        })
       } catch (error) {
-        console.log('SIXPACK PARTICIPATE ERROR: ', error)
+        console.error('SIXPACK PARTICIPATE ERROR: ', error)
+        return { error: true, message: error }
       }
     })
   }
 
-  async convertEvent ({
-    kpi,
-    session,
-    experimentName
-  }: Exp.ConvertParams): Promise<Exp.ConvertResponse> {
-    return new Promise<any>((resolve, reject) => {
+  async convertEvent (params: Exp.ConvertParams): Promise<Exp.ConvertResponse> {
+    const { kpi, session, experimentName } = params
+    return new Promise<Exp.ConvertResponse>((resolve) => {
       try {
-        if (!session) return console.error('[CONVERT] SESSION NOT FOUND')
+        if (!session) return console.error('[CONVERT] SESSION NOT FOUND');
 
-        session.convert(experimentName, kpi, (_null: null, result: any): any => {
+        (session as any).convert(experimentName, kpi, (_null: null, result: any): any => {
           if (result?.status) {
             return resolve({
-              status: result?.status,
+              success: true,
               kpi: result?.conversion?.kpi,
               experimentName: result?.experiment?.name,
               alternativeName: result?.alternative?.name
@@ -69,7 +58,8 @@ export class SixpackExperiments implements Exp.ExperimentsParams {
           }
         })
       } catch (error) {
-        console.log('SIXPACK CONVERT ERROR: ', error)
+        console.error('SIXPACK CONVERT ERROR: ', error)
+        return { error: true, message: error }
       }
     })
   }
