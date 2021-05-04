39959980c62effdb8ff935af4b8f6d42059ae52b
felipelealdefaria <felipelealdefaria@gmail.com>
2021-05-04 15:11:41 -0300

    feat: formatted kpi name

--- src/data/experiments-service.ts
+++ src/data/experiments-service.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import * as Exp from '@/domain/experiments'
 import { ExperimentsParams } from '@/data/experiments-params'
 
+const slugify = require('slugify')
 export class ExperimentsService implements Exp.Experiments {
   baseUrl = 'http://127.0.0.1:5000'
   constructor (
@@ -18,7 +20,7 @@ export class ExperimentsService implements Exp.Experiments {
   async participate ({ traffic, session, variationsName, experimentName }: Exp.ParticipateParams): Promise<Exp.ParticipateResponse> {
     if (!session) session = await this.init({ baseUrl: this.baseUrl })
 
-    const force = this.forceVariant(`force-${experimentName}`)
+    const force: string | null = this.forceVariant(`force-${experimentName}`)
     const res: Exp.ParticipateResponse = await this.experiment.participateExperiment({ force, session, experimentName, variationsName, traffic: traffic || 1 })
 
     if (res?.success) return { success: true, experimentName: res?.experimentName, alternativeName: res?.alternativeName }
@@ -27,18 +29,29 @@ export class ExperimentsService implements Exp.Experiments {
 
   async convert ({ kpi, session, experimentName }: Exp.ConvertParams): Promise<Exp.ConvertResponse> {
     if (!session) session = await this.init({ baseUrl: this.baseUrl })
-    const res: Exp.ConvertResponse = await this.experiment.convertEvent({ kpi, session, experimentName })
+
+    let kpiFormatted: string | undefined = kpi
+    if (kpi) kpiFormatted = this.formattedString(kpi)
+    const res: Exp.ConvertResponse = await this.experiment.convertEvent({ kpi: kpiFormatted, session, experimentName })
 
     if (res?.success) return { success: true, kpi: res?.kpi, experimentName: res?.experimentName, alternativeName: res?.alternativeName }
     return { error: true, message: res?.message }
   }
 
-  forceVariant (cookieName: string): any {
-    const decodedCookies = decodeURIComponent(document.cookie)
-    const cookies = decodedCookies.split(';')
-    const forceCookie = cookies.find((cookie) => cookie.match(cookieName))
+  forceVariant (cookieName: string): string | null {
+    const decodedCookies: string = decodeURIComponent(document.cookie)
+    const cookies: string[] = decodedCookies.split(';')
+    const forceCookie: string | undefined = cookies.find((cookie) => cookie.match(cookieName))
 
     if (forceCookie) return forceCookie.replace(`${cookieName}=`, '').trim()
-    return forceCookie || null
+    return null
+  }
+
+  formattedString (kpi: string): string {
+    try {
+      return slugify(kpi, { lower: true, strict: true, replacement: '_' })
+    } catch (error) {
+      return kpi.replace(/\s/g, '_').toLowerCase()
+    }
   }
 }
