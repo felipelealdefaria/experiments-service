7730cd718fd38fe5e94775b00b6e0c347476d932
felipelealdefaria <felipelealdefaria@gmail.com>
2021-05-04 15:12:34 -0300

    chore: returning status for service

--- src/infra/sixpack-experiments.ts
+++ src/infra/sixpack-experiments.ts
@@ -7,7 +7,8 @@ export class SixpackExperiments implements Exp.ExperimentsParams {
   async startExperiment ({ baseUrl, timeout }: Exp.StartExperimentParams): Promise<Exp.StartExperimentResponse> {
     try {
       const res = await new sixpack.Session({ base_url: baseUrl, timeout: timeout || 8000 })
-      return { success: true, session: res }
+      if (res) return { success: true, session: res }
+      return { error: true, session: null }
     } catch (error) {
       console.error('SIXPACK INIT ERROR: ', error)
       return { error: true, session: null }
@@ -18,10 +19,16 @@ export class SixpackExperiments implements Exp.ExperimentsParams {
     const { force, traffic, session, experimentName, variationsName } = params
     return new Promise<Exp.ParticipateResponse>((resolve, reject) => {
       try {
-        if (!session) return console.error('[PARTICIPATE] SESSION NOT FOUND');
+        if (!session) {
+          console.error('[PARTICIPATE] SESSION NOT FOUND')
+          return { error: true, message: '[PARTICIPATE] SESSION NOT FOUND' }
+        }
 
         (session as any).participate(experimentName, variationsName, traffic, force, (error: any, result: any): any => {
-          if (error) return reject(error)
+          if (error) {
+            console.error('[PARTICIPATE] PROMISE ERROR')
+            return reject(error)
+          }
           if (result) {
             if (result.status === 'failed') {
               console.error('[PARTICIPATE] REQUEST ERROR: ', result?.error)
@@ -45,10 +52,13 @@ export class SixpackExperiments implements Exp.ExperimentsParams {
     const { kpi, session, experimentName } = params
     return new Promise<Exp.ConvertResponse>((resolve) => {
       try {
-        if (!session) return console.error('[CONVERT] SESSION NOT FOUND');
+        if (!session) {
+          console.error('[CONVERT] SESSION NOT FOUND')
+          return { error: true, message: '[CONVERT] SESSION NOT FOUND' }
+        }
 
         (session as any).convert(experimentName, kpi, (_null: null, result: any): any => {
-          if (result?.status) {
+          if (result?.status === 'ok') {
             return resolve({
               success: true,
               kpi: result?.conversion?.kpi,
@@ -56,6 +66,7 @@ export class SixpackExperiments implements Exp.ExperimentsParams {
               alternativeName: result?.alternative?.name
             })
           }
+          return { error: true, message: '[CONVERT] FUNCTION ERROR' }
         })
       } catch (error) {
         console.error('SIXPACK CONVERT ERROR: ', error)
